7a0a10358a7b832b54df515a5c2abd5460b4698d
maskrebel <33626739+maskrebel@users.noreply.github.com>
2024-10-18 15:45:11 +0700

    feat: update image Carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,43 +1,27 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
 import './Carousel.css'
+import settings from "./Settings.tsx";
+
 
 const CustomCarousel: React.FC = () => {
+    const images = settings.imageCarousel;
     return (
         <Carousel>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src="https://via.placeholder.com/800x400?text=Image+1"
-                    alt="First slide"
-                />
-                <Carousel.Caption>
-                    <h3>First Slide</h3>
-                    <p>Description for the first slide.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src="https://via.placeholder.com/800x400?text=Image+2"
-                    alt="Second slide"
-                />
-                <Carousel.Caption>
-                    <h3>Second Slide</h3>
-                    <p>Description for the second slide.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src="https://via.placeholder.com/800x400?text=Image+3"
-                    alt="Third slide"
-                />
-                <Carousel.Caption>
-                    <h3>Third Slide</h3>
-                    <p>Description for the third slide.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
+            {images.map((URL, index) => (
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        alt={URL}
+                        src={URL}
+                        key={index}
+                    />
+                    <Carousel.Caption>
+                        <h3>Slide {index}</h3>
+                        <p>Description for the slide {index}.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            ))}
         </Carousel>
     );
 };
--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -9,6 +9,11 @@ class Settings {
     static urlLocationMap = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3967.0378603952263!2d106.57914407316575!3d-6.125607560059214!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6a01086e589757%3A0x5e8a2a53a33e5589!2sFumikos!5e0!3m2!1sen!2sid!4v1729234978666!5m2!1sen!2sid'
     static urlTokopedia = 'https://www.tokopedia.com/fumikos'
     static urlShopee = 'https://shopee.co.id/fumikos.id'
+    static imageCarousel = [
+        "https://res.cloudinary.com/demo/image/upload/v1652345767/docs/demo_image2.jpg",
+        "https://res.cloudinary.com/demo/image/upload/v1652366604/docs/demo_image5.jpg",
+        "https://res.cloudinary.com/demo/image/upload/v1652345874/docs/demo_image1.jpg",
+    ]
 }
 
 export default Settings;
